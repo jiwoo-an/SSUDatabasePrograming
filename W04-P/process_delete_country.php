3827948782d69f8cc503c442b459e736e04f3325
kanjani8 <42990328+kanjani8@users.noreply.github.com>
2020-09-26 17:07:20 +0900

    Update process_delete_country.php

--- W04-P/process_delete_country.php
+++ W04-P/process_delete_country.php
@@ -10,9 +10,13 @@
       FROM country
       WHERE id = '{$filtered['id']}'
   ";
+  $query_delete_language = "
+    DELETE FROM language WHERE country_id = '{$filtered['id']}'
+  ";
 
 
-  $result = mysqli_query($link, $query);
+  $result = mysqli_query($link, $query); // 나라 삭제
+  $result2 = mysqli_query($link, $query_delete_language); // 연결된 언어 삭제
   if ($result == false) {
       echo '삭제하는 과정에서 문제가 발생했습니다. 관라자에게 문의하세요.';
       error_log(mysqli_error($link));
